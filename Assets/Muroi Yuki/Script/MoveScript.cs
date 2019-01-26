c5c12af62d902e6045c85a6c6056b2e36aa3727f
室井　優希 <zeal17300037@c-league.jp>
2019-01-26 13:19:15 +0900

    MoveScript.csを追加

---
+++ Assets/Muroi Yuki/Script/MoveScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 2.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //横に移動
+        if(Input.GetAxis("Horizontal") != 0)
+        {
+            transform.position -= new Vector3((Input.GetAxis("Horizontal") * speed * Time.deltaTime), 0.0f, 0.0f);
+        }
+
+        //縦に移動
+        if(Input.GetAxis("Vertical") != 0)
+        {
+            transform.position += new Vector3(0.0f, 0.0f, (Input.GetAxis("Vertical") * speed * Time.deltaTime));
+        }
+
+
+    }
+}
