f9707c81bc42e9bb8c3350498290f047fd5cb519
Onami Masaki <zeal17300024@c-league.jp>
2019-01-27 16:10:07 +0900

    マージ。

---
+++ Assets/Inoyu/Enemy/EnemySerch.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySerch : MonoBehaviour
+{
+
+    public float move_speed = 3.0f;
+
+    protected Rigidbody rb;
+    protected Transform followTarget;
+    protected MoveMode CurrentMoveMode;
+
+    public enum MoveMode
+    {
+        Idle = 1,
+        Follow = 2,
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        CurrentMoveMode = MoveMode.Idle;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DoAutoMovement();
+    }
+
+    protected void DoAutoMovement()
+    {
+        switch (CurrentMoveMode)
+        {
+            case MoveMode.Idle:
+                break;
+            case MoveMode.Follow:
+                if (followTarget != null)
+                {
+                    Quaternion move_rotation = Quaternion.LookRotation(followTarget.transform.position - transform.position, Vector3.up);
+                    transform.rotation = Quaternion.Lerp(transform.rotation, move_rotation, 0.1f);
+                    rb.velocity = transform.forward * move_speed;
+                }
+
+                break;
+        }
+    }
+
+    public void OnEnterFollowTarget()
+    {
+        followTarget = null;
+
+        if (CurrentMoveMode == MoveMode.Follow)
+        {
+            CurrentMoveMode = MoveMode.Idle;
+        }
+    }
+
+    public void OnExitFollowTarget(Transform target)
+    {
+        followTarget = target;
+
+        if (CurrentMoveMode == MoveMode.Idle)
+        {
+            CurrentMoveMode = MoveMode.Follow;
+        }
+    }
+}
